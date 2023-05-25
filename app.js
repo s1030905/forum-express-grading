6872835261eb48f13974db93582d58ed8384ebd5
s1030905 <s1030905@outlook.com>
2023-05-25 20:15:36 +0800

    Internal Server Error 1

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
-  // res.locals.error_msg = req.flash('error_msg')
   res.locals.user = getUser(req)
   next()
 })
